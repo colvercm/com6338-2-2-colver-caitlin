87883107c67901e4a235c998a50e38d0d83b542d
Caitlin Colver <colvercm@gmail.com>
2023-05-28 16:19:29 -0600

    added name & age

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+
+function greet(name) {
+    var name = prompt('What is your name?');
+
+    if (name && name.trim) {
+        var trimmedName = name.trim()
+        alert('Hello, ' + trimmedName);
+    } else {
+        alert('Hello')
+    }
+    
+    
+
+
+    
+}
+
+
+
+
+
+
+    
+
+
+
+
+
